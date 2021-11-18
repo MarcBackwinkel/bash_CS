f7b4b464dbaac447f77cd69365708ffdd1cec4b1
MarcBackwinkel <marc.backwinkel@gmail.com>
2021-11-18 14:09:53 +0000

    .

--- standard_bash.sh
+++ standard_bash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Die ersten beiden Zeichen oben #! besagen, dass die Datei mit dem Programm "bin/bash" ausgefuehrt werden sollen
+
 # vor der ersten Ausfuehrung der Datei folgende Schritte umsetzen:
 # chmod +x standard_bash.sh         # der Datei Ausfuehrungsrechte geben
 # ls -l standard_bash.sh            # Check, ob Ausfuehrungsrechte gesetzt wurden
